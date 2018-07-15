89eeb2824a419db5f2fe648b9ad7e1accd1c11c1
1600133971 <34409521+1600133971@users.noreply.github.com>
2018-07-15 13:56:12 +0800

    add fixture and failed test rerun

--- test/app.js
+++ test/app.js
@@ -3,25 +3,25 @@
 let createTestCafe = require('testcafe');
 let fs = require('fs');
 let testcafe = null;
-let runner = null;
 
-let listFailedTestName = function(path) {
-  let last = JSON.parse('[' + fs.readFileSync(path).toString().replace(/}{/g, '},{') + ']').pop();
-  let list = new Array()
-  last.fixtures.forEach(function(fixture, index) {
+let checkFailedTest = function(testName, fixtureName, logPathName) {
+  let flag = false;
+  JSON.parse('[' + fs.readFileSync(logPathName).toString().replace(/}{/g, '},{') + ']').pop().fixtures.forEach(function(fixture, index) {
     fixture.tests.forEach(function(test, index) {
-      if (test.errs.length > 0) list.push({fixture: fixture.name, test: test.name});
+      if (test.errs.length > 0)
+        flag = flag || (fixture.name == fixtureName && test.name == testName);
     });
   });
-  return list;
+  return flag;
 }
 
 createTestCafe('localhost')
   .then(tc => {
     testcafe = tc;
-    runner = testcafe.createRunner();
+    let runner = testcafe.createRunner();
     let jsonFilePath = 'report_' + Date.now() + '.json';
     let jsonStream = fs.createWriteStream(jsonFilePath);
+    let htmlStream = fs.createWriteStream('report_' + Date.now() + '.html');
     return runner
       .startApp('node server.js 8085', 4000)
       .src('fixture1.js')
@@ -29,74 +29,49 @@ createTestCafe('localhost')
       .src('fixture3.js')
       .src('fixture4.js')
       .src('fixture5.js')
+      .src('fixture6.js')
       .browsers('chrome:headless')
       .reporter('st')
       .reporter('json', jsonStream)
-      .concurrency(3)
+      .reporter('st-html', htmlStream)
+      .concurrency(4)
       .screenshots('./')
       .run()
       .then(failedCount => {
-        let ls = listFailedTestName(jsonFilePath);
-        if (failedCount > 0 && ls.length > 0) {
+        if (failedCount > 0) {
           return runner
-            .concurrency(1)
-            .filter((testName, fixtureName, fixturePath) => {
-              let flag = false;
-              ls.forEach(function(item, index) {
-                flag = flag || (item.fixture == fixtureName && item.test == testName);
-              });
-              return flag;
-            })
+            .concurrency(3)
+            .filter((testName, fixtureName, fixturePath) => {return checkFailedTest(testName, fixtureName, jsonFilePath);})
             .run();
         }
       })
       .then(failedCount => {
-        let ls = listFailedTestName(jsonFilePath);
-        if (failedCount > 0 && ls.length > 0) {
+        if (failedCount > 0) {
           return runner
-            .concurrency(1)
-            .filter((testName, fixtureName, fixturePath) => {
-              let flag = false;
-              ls.forEach(function(item, index) {
-                flag = flag || (item.fixture == fixtureName && item.test == testName);
-              });
-              return flag;
-            })
+            .concurrency(2)
+            .filter((testName, fixtureName, fixturePath) => {return checkFailedTest(testName, fixtureName, jsonFilePath);})
             .run();
         }
       })
       .then(failedCount => {
-        let ls = listFailedTestName(jsonFilePath);
-        if (failedCount > 0 && ls.length > 0) {
+        if (failedCount > 0) {
           return runner
             .concurrency(1)
-            .filter((testName, fixtureName, fixturePath) => {
-              let flag = false;
-              ls.forEach(function(item, index) {
-                flag = flag || (item.fixture == fixtureName && item.test == testName);
-              });
-              return flag;
-            })
+            .filter((testName, fixtureName, fixturePath) => {return checkFailedTest(testName, fixtureName, jsonFilePath);})
             .run();
         }
       })
       .then(failedCount => {
-        let ls = listFailedTestName(jsonFilePath);
-        if (failedCount > 0 && ls.length > 0) {
+        if (failedCount > 0) {
           return runner
             .concurrency(1)
-            .filter((testName, fixtureName, fixturePath) => {
-              let flag = false;
-              ls.forEach(function(item, index) {
-                flag = flag || (item.fixture == fixtureName && item.test == testName);
-              });
-              return flag;
-            })
+            .filter((testName, fixtureName, fixturePath) => {return checkFailedTest(testName, fixtureName, jsonFilePath);})
             .run();
         }
       })
       .then(failedCount => {
         jsonStream.end();
+        htmlStream.end();
       });
   })
   .then(failedCount => {
