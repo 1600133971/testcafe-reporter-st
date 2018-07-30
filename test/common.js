464cf9cb7fed03e40ec8281e812fd88300af6d5c
1600133971 <34409521+1600133971@users.noreply.github.com>
2018-07-30 23:15:32 +0800

    add common

--- test/common.js
+++ test/common.js
@@ -33,5 +33,59 @@ export let f = {
   waitForDisappear: async function(selector, ms) {
     let sl = f.getSelector(selector);
     await t.expect(sl.with({visibilityCheck: true}).nth(0).exists).notOk({timeout: ms});
-  }
+  },
+
+  selectOption: async function(selector, text) {
+    let sl = f.getSelector(selector);
+    await t
+      .hover(sl)
+      .expect(sl.hasAttribute("disabled")).notOk({timeout: 5000})
+      .click(sl)
+      .click(sl.find("option").withText(text).nth(0))
+  },
+
+  selectFirstOption: async function(selector) {
+    let sl = f.getSelector(selector);
+    await t
+      .hover(sl)
+      .expect(sl.hasAttribute("disabled")).notOk({timeout: 5000})
+      .click(sl)
+      .click(sl.find("option").nth(0))
+  },
+
+  selectFirstNonEmptyOption: async function(selector) {
+    let sl = f.getSelector(selector);
+    let firstNonEmptyOption = selector
+      .find("option")
+      .filter((node) => {
+        const option = node;
+        if (option && option.innerText && option.innerText.trim() !== "") {
+            return true;
+        }
+        return false;
+      })
+      .nth(0);
+    await t
+      .hover(sl)
+      .click(sl)
+      .click(firstNonEmptyOption);
+  },
+
+  selectLastOption: async function(selector) {
+    let sl = f.getSelector(selector);
+    await t
+      .hover(sl)
+      .expect(sl.hasAttribute("disabled")).notOk({timeout: 5000})
+      .click(sl)
+      .click(sl.find("option").nth(-1))
+  },
+
+  selectExactOption: async function(selector, exactText) {
+    let sl = f.getSelector(selector);
+    await t
+      .hover(sl)
+      .expect(sl.hasAttribute("disabled")).notOk({timeout: 5000})
+      .click(sl)
+      .click(sl.find("option").withExactText(exactText).nth(0))
+  },
 };
