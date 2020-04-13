512fce17dec989809669eecc4af3c4a991aa6646
1600133971 <34409521+1600133971@users.noreply.github.com>
2020-04-13 21:14:12 +0800

    add

--- test/chain.js
+++ test/chain.js
@@ -159,6 +159,18 @@ class ExtendCmd extends TestCafeCmd {
   async test(timeout = 1000) {
     await t.wait(timeout);
   }
+
+  async test1(timeout = 1000) {
+    await this.test(timeout);
+  }
+
+  async test2(timeout = 1000) {
+    await this.test2(timeout);
+  }
+
+  async foo () {
+    return 'foo'
+  }
 }
 
 export let f = chain(new ExtendCmd());
--- test/fixtures/fixture20.js
+++ test/fixtures/fixture20.js
@@ -8,8 +8,11 @@ fixture`Getting test`
 test('My test 12', async t => {
   await f
     .typeText('#developer-name', 'John Smith')
+    .test2()
     .click('#submit-button')
     .test()
     .eql(Selector('#article-header').innerText, 'Thank you, John Smith!')
+    .foo().result((v) => console.log(v))
+    .test1()
     .end();
 });
